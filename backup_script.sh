72a4745346b0e65b7b16e80ef81ea89cc6731e96
Oluwadt <dtonilede@gmail.com>
2025-11-06 14:08:32 +0100

    Added file configuration

--- backup_script.sh
+++ backup_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # Automated Backup Script
 
+SOURCE_DIRS=("/home/user/Documents" "/home/user/Pictures")
+BACKUP_DIR="/home/user/backups"
+LOG_FILE="$BACKUP_DIR/backup.log"
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M-%S)
+BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
 
