09b98b34c844f4647ea1b5f46422dd9c7aa88e14
Oluwadt <oluwadt@DESKTOP-P7I9SNN.localdomain>
2025-11-06 10:39:14 +0100

    Added check_disk and check_network functions

--- system_health_check.sh
+++ system_health_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # System Health Check Script
 
-# Get CPU usage
+# Get CPU Usage
 check_cpu() {
     echo "CPU Load:"
     uptime | awk -F'load average:' '{ print $2 }'
@@ -10,10 +10,26 @@ check_cpu() {
 
 # Get Memory Usage
 check_memory () {
-    echo "Menory Usage"
+    echo "Memory Usage"
     free -h | awk '/Mem:/ {print "Used: "$3" | Free: "$4" | Total: "$2}'
+    echo
+}
+
+# Get Disk Usage
+check_disk () {
+    echo "Disk Usage:"
+    df -h --total | awk '/total/ { print "Used: "$3" | Free: "$4" | Usage: "$5 }'
+    echo
+}
+
+# Get Network Info
+check_network () {
+    echo "Network Information:"
+    ip -br address | awk '{ print $1": "$3 }'
     echo
 }
 
 check_cpu
 check_memory
+check_disk
+check_network
