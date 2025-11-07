4260141eac608e584972d2d7dcd0ef7238374867
Oluwadt <dtonilede@gmail.com>
2025-11-07 13:28:02 +0100

    Added check_service and log_message functions

--- monitor_service.sh
+++ monitor_service.sh
@@ -6,4 +6,16 @@ LOG_FILE="service_check.log"
 EMAIL="admin@company.com"
 CHECK_INTERVAL=60
 
+log_message () {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S') - $1]" >> "$LOG_FILE"
+}
 
+check_service () {
+    local SERVICE=$1
+    curl -Is --max-time 10 "$SERVICE" > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        log_message "$SERVICE is UP"
+    else
+        log_message "$SERVICE is DOWN"
+    fi
+}
