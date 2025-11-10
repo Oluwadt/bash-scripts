d01022f3e348222c306faf1ceb6cc2eb8807d271
Oluwadt <dtonilede@gmail.com>
2025-11-10 10:30:58 +0100

    Created project folder

--- env_setup.sh
+++ env_setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 # Environment Setup Script
 
+# Update package list
+sudo apt update
 
+# Install Git
+sudo apt install -y git
+
+# Install Python3 and pip
+sudo apt install -y python3 python3-pip
+
+# Create project directory
+mkdir -p ~/projects/my_project
+
+# Navigate to project directory
+cd ~/projects/my_project
